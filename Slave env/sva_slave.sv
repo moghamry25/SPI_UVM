1d761e8f01e343d6493c0192ea7004393709b4ff
moghamry25 <mahmoudghamry2001@gmail.com>
2025-06-14 20:49:11 +0300

    SVA Addition

---
+++ Slave env/sva_slave.sv
@@ -0,0 +1,16 @@
+module sva_slave(
+    input clk,
+    input logic rst_n,
+    input logic MOSI,
+    input logic SS_n,
+    input logic tx_valid,
+    input logic [7:0] tx_data,
+    input logic MISO,
+    input logic rx_valid,
+    input logic [9:0] rx_data
+
+
+);
+    
+
+endmodule
--- Slave env/top_slave.sv
+++ Slave env/top_slave.sv
@@ -15,7 +15,18 @@ import uvm_pkg::*;
     slave DUT(if_slave.MOSI,if_slave.MISO, if_slave.SS_n,if_slave.clk, if_slave.rst_n, if_slave.rx_data,if_slave.rx_valid,if_slave.tx_data, if_slave.tx_valid);
     golden_slave golden(if_slave.MOSI, 
     if_slave.clk, if_slave.rst_n,if_slave.SS_n,if_slave.tx_valid,if_slave.tx_data,if_slave.MISO_ref,if_slave.rx_valid_ref, if_slave.rx_data_ref);
-  
+    
+    bind slave sva_slave sva_slave_inst(
+        if_slave.clk,
+        if_slave.rst_n,
+        if_slave.MOSI,
+        if_slave.SS_n,
+        if_slave.tx_valid,
+        if_slave.tx_data,
+        if_slave.MISO,
+        if_slave.rx_valid,
+        if_slave.rx_data
+    );
     initial begin
 
         uvm_config_db#(virtual interface_slave)::set(null, "uvm_test_top", "vif", if_slave);
