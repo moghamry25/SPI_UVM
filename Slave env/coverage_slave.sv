ea6ee7539655767f8b002c258d1a397723ee7e2f
moghamry25 <mahmoudghamry2001@gmail.com>
2025-06-14 20:42:32 +0300

    Functional coverage for slave

--- Slave env/coverage_slave.sv
+++ Slave env/coverage_slave.sv
@@ -10,10 +10,50 @@ class coverage_slave extends uvm_component;
     uvm_analysis_export#(sequence_slave_item) cov_export;
     uvm_tlm_analysis_fifo#(sequence_slave_item) cov_fifo;
 
+    // functional coverage 
+    // uvm_covergroup cov1;
+
+    covergroup cov1;
+
+    Covering_rx_data:coverpoint item.rx_data {
+        option.comment = "Covering rx_data";
+        bins data_bins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+   Covering_rx_valid: coverpoint item.rx_valid {
+        option.comment = "Covering rx_valid";
+        bins valid_bins[] = {0, 1};
+    }
+    Covering_MISO:coverpoint item.MISO {
+        option.comment = "Covering MISO";
+        bins miso_bins[] = {0, 1};
+    }
+    Covering_SS_n:coverpoint item.SS_n {
+        option.comment = "Covering SS_n";
+        bins ss_bins[] = {0, 1};
+    }
+
+    Covering_tx_valid:coverpoint item.tx_valid {
+        option.comment = "Covering tx_valid";
+        bins tx_valid_bins[] = {0, 1};
+    }
+
+    Covering_tx_data:coverpoint item.tx_data {
+        option.comment = "Covering tx_data";
+        bins tx_data_bins[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    }
+    Covering_rst_n:coverpoint item.rst_n {
+        option.comment = "Covering rst_n";
+        bins rst_bins[] = {0, 1};
+    }
+    
+    
+
+    endgroup
+
     // Constructor
     function new(string name = "coverage_slave", uvm_component parent = null);
         super.new(name, parent);
-        
+        cov1 = new();
     endfunction
 
    function void build_phase(uvm_phase phase);
@@ -33,7 +73,7 @@ class coverage_slave extends uvm_component;
         forever begin
             // Wait for an item to be written to the FIFO
         cov_fifo.get(item);
-           //cov1.sample();
+        cov1.sample();
            
             
         end
